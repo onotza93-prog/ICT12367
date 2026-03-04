d53435ef11a6c9746c0d4ccaedf1132bc1846fc4
onotza93-prog <onotza93@gmail.com>
2026-03-04 23:52:05 +0700

    Add files via upload

---
+++ assignment6/assignment6.py
@@ -0,0 +1,11 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> age = 19
+>>> print("อายุของฉันคือ", age)
+อายุของฉันคือ 19
+>>> name = "Anuwat Rungsin"
+>>> age = 19
+>>> print(f"สวัสดี {name}, อายุ {age} ปี")
+สวัสดี Anuwat Rungsin, อายุ 19 ปี
+>>> print("Python", "Java", "C++", sep=" | ")
+Python | Java | C++
